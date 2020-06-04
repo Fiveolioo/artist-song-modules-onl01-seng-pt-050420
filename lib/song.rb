e07a8e097b3eb833005c5a26510819bdbac4da93
Richard Cho <96richardohc@gmail.com>
2020-06-04 00:30:15 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -5,17 +5,13 @@ class Song
   extend Memorable::ClassMethods
   extend Findable::ClassMethods
   include Paramable::InstanceMethods
+  include Memorable::InstanceMethods
   
   attr_accessor :name
   attr_reader :artist
   
   
   @@songs = []
-
-  def initialize
-    @@songs << self
-  end
-
 
   def self.all
     @@songs
